316ae636947cd838bd8be4ea470127ab20fdc42d
SableMeow <105553471+YashSoPro@users.noreply.github.com>
2024-10-22 20:09:23 +0530

    Update CommentsAdapter.java

--- app/src/main/java/awais/instagrabber/adapters/CommentsAdapter.java
+++ app/src/main/java/awais/instagrabber/adapters/CommentsAdapter.java
@@ -14,14 +14,17 @@ import awais.instagrabber.databinding.ItemCommentBinding;
 import awais.instagrabber.models.Comment;
 
 public final class CommentsAdapter extends ListAdapter<Comment, CommentViewHolder> {
+    // Callback for handling the difference in items
     private static final DiffUtil.ItemCallback<Comment> DIFF_CALLBACK = new DiffUtil.ItemCallback<Comment>() {
         @Override
         public boolean areItemsTheSame(@NonNull final Comment oldItem, @NonNull final Comment newItem) {
+            // Check if the primary keys of the comments are the same
             return Objects.equals(oldItem.getPk(), newItem.getPk());
         }
 
         @Override
         public boolean areContentsTheSame(@NonNull final Comment oldItem, @NonNull final Comment newItem) {
+            // Check if the content of the comments is the same
             return Objects.equals(oldItem, newItem);
         }
     };
@@ -41,7 +44,8 @@ public final class CommentsAdapter extends ListAdapter<Comment, CommentViewHolde
 
     @NonNull
     @Override
-    public CommentViewHolder onCreateViewHolder(@NonNull final ViewGroup parent, final int type) {
+    public CommentViewHolder onCreateViewHolder(@NonNull final ViewGroup parent, final int viewType) {
+        // Inflate the layout for each comment item
         final LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         final ItemCommentBinding binding = ItemCommentBinding.inflate(layoutInflater, parent, false);
         return new CommentViewHolder(binding, currentUserId, commentCallback);
@@ -50,28 +54,21 @@ public final class CommentsAdapter extends ListAdapter<Comment, CommentViewHolde
     @Override
     public void onBindViewHolder(@NonNull final CommentViewHolder holder, final int position) {
         final Comment comment = getItem(position);
+        // Bind the comment to the ViewHolder
         holder.bind(comment, showingReplies && position == 0, showingReplies && position != 0);
     }
 
+    // Interface for handling comment actions
     public interface CommentCallback {
         void onClick(final Comment comment);
-
         void onHashtagClick(final String hashtag);
-
         void onMentionClick(final String mention);
-
         void onURLClick(final String url);
-
         void onEmailClick(final String emailAddress);
-
         void onLikeClick(final Comment comment, boolean liked, final boolean isReply);
-
         void onRepliesClick(final Comment comment);
-
         void onViewLikes(Comment comment);
-
         void onTranslate(Comment comment);
-
         void onDelete(Comment comment, boolean isReply);
     }
 }
